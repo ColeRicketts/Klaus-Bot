5a305c9aec20f38301f7ade4f41b641370b314c7
Ded-Ex <61940751+Ded-Ex@users.noreply.github.com>
2020-08-08 14:37:46 +0100

    Changed colour

--- cogs/wikipedia.py
+++ cogs/wikipedia.py
@@ -26,7 +26,7 @@ class Wikipedia(commands.Cog):
         request = " ".join(request)
         error = None
         definition = wikipedia.summary(request, sentences=2, chars=1000, auto_suggest=True, redirect = True)
-        search=discord.Embed(title="Wikipedia Result", description=definition, colour=discord.Colour.white())
+        search=discord.Embed(title="Wikipedia Result", description=definition, colour=discord.Colour.dark_green())
         await ctx.send(content=None, embed=search)
         
 def setup(client):
