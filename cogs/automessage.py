03410455d3ce2f1b04cdc7c66b1937077fb11a2f
Ded-Ex <61940751+Ded-Ex@users.noreply.github.com>
2020-09-22 15:16:10 +0100

    Created automessage.py, which announces when a user joins or leaves the server in a dedicated welcome channel.

--- cogs/automessage.py
+++ cogs/automessage.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
+from time import ctime
+import json
 client = commands.Bot(command_prefix='!')
+
+with open('config.json') as configFile:
+    data = json.load(configFile)
+    for value in data["server_details"]:
+        welcome_id = value['welcome_id']
 
 class Moderation(commands.Cog):
     def __init__(self, client):
@@ -9,6 +16,23 @@ class Moderation(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('Automessage Online!')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.client.get_channel(int(welcome_id))
+        embedVar = discord.Embed(title="USER JOINED:", description="User: **{0}**".format(member.mention), color=0x979c9f)
+        embedVar.add_field(name="Time:", value="**{0}**".format(ctime()), inline=True)
+        embedVar.set_footer(text="Welcome to the server!")
+        await channel.send(embed=embedVar)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = self.client.get_channel(int(welcome_id))
+        embedVar = discord.Embed(title="USER LEFT:", description="User: **{0}**".format(member.mention),
+                                 color=0x979c9f)
+        embedVar.add_field(name="Time:", value="**{0}**".format(ctime()), inline=True)
+        embedVar.set_footer(text="Goodbye!")
+        await channel.send(embed=embedVar)
 
 def setup(client):
     client.add_cog(Moderation(client))
